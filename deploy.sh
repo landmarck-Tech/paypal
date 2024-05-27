e87fe30fd2198c182d42e75815063907a4bb45dd
chidi <dhammondg@gmail.com>
2024-05-26 22:37:15 -0500

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 This application is about to be deployed
 make sure this application is reviewed
 by our managers before distribution
-This procedure must be followed
+This procedure must be followed.
+No room for excuses. culprits shall be dealt with
+Thank you engineers
