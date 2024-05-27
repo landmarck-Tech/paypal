fa3e8aa0b81e4acab60bee27cec2061c297c590e
chidi <dhammondg@gmail.com>
2024-05-26 22:44:33 -0500

    modification

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ This application is about to be deployed
 make sure this application is reviewed
 by our managers before distribution
 This procedure must be followed
+so the processes will be smooth
